23bcd7bccdcd530089a3d874d3116a317c37b9f7
david1alvarez <dalvarez@predictiveindex.com>
2020-04-25 18:35:21 -0700

    Updates

---
+++ precache-manifest.b0e8585dd0453d80d4ea6f9ed800be55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2560af1a85b4f366fb03688050828892",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f821386100da3f0c2af8",
+    "url": "/covid19/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "ed4f822c9d13c723ba09",
+    "url": "/covid19/static/css/main.6db80fb5.chunk.css"
+  },
+  {
+    "revision": "f821386100da3f0c2af8",
+    "url": "/covid19/static/js/2.56dcb70b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.56dcb70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4f822c9d13c723ba09",
+    "url": "/covid19/static/js/main.c13f1136.chunk.js"
+  },
+  {
+    "revision": "bb08661687ef9ed76af2",
+    "url": "/covid19/static/js/runtime-main.06ba182b.js"
+  }
+]);
