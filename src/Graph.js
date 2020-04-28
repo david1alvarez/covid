aaac31acd02f5665555aee3ac720459f3d0f760a
david1alvarez <dalvarez@predictiveindex.com>
2020-04-27 17:22:20 -0700

    get d3 graphing

--- src/Graph.js
+++ src/Graph.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import '../node_modules/react-vis/dist/style.css';
-import { XYPlot, LineSeries } from 'react-vis';
 import * as d3 from 'd3';
 import './Graph.css';
-
-/*
-    TODO:
-    pass in the data as part of the props, and only render the graph when there is actual data
-    this avoids the necessity of having a million "If the data exists, do x" lines of code
-*/
+import LineChart from './LineChart';
 
 class Graph extends Component {
     constructor() {
         super();
-        this.state = {data: []};
+        this.state = {data: [], graphData: {}};
 
         this.GetWorldStatistics = this.GetWorldStatistics.bind(this);
     }
 
     componentDidMount() {
-        this.GetWorldStatistics();
+        this.GetWorldStatistics()
+
+        this.setState({graphData: {
+            points: [
+                // cumulativedata needs to return the dates, not the numbers
+                this.cumulativeData(this.state.data),
+                this.cumulativeData(this.derive(this.state.data, 1))
+            ],
+            xValues: this.state.data.map(datum => {return this.getDate(datum.date)}),
+            minDate: this.getDate(d3.min(this.state.data, function(datum) {return datum.date})),
+            maxDate: this.getDate(d3.max(this.state.data, function(datum) {return datum.date})),
+            yMin: d3.min(this.state.data, function(datum) {return datum.positive}),
+            yMax: d3.max(this.state.data, function(datum) {return datum.positive})
+        }})
     }
 
     GetWorldStatistics() {
@@ -27,20 +34,28 @@ class Graph extends Component {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }} )
-            .then((response) =>{
-                return response.json();
-            })
-            .then(json => {
-                this.setState({ data: json});
-                this.render()
-            })
+        .then((response) =>{
+            return response.json();
+        })
+        .then(json => {
+            // this.setState({ data: json});
+            this.setState({ data: json, graphData: {
+                points: [
+                    this.cumulativeData(json)//,
+                    // this.cumulativeData(this.derive(json, 1))
+                ],
+                xValues: json.map(datum => {return this.getDate(datum.date)}),
+                minDate: this.getDate(d3.min(json, function(datum) {return datum.date})),
+                maxDate: this.getDate(d3.max(json, function(datum) {return datum.date})),
+                yMin: d3.min(json, function(datum) {return datum.positive}),
+                yMax: d3.max(json, function(datum) {return datum.positive})
+            }})
+            this.render()
+        })
     }
 
     // convert yyyymmdd integer to Date object
     getDate(numberDate) {
-        if (!numberDate) {
-            return null;
-        }
         const year = Math.floor(numberDate / 10000);
         const month = Math.floor((numberDate) / 100) - 1 - (year * 100);
         const day = numberDate - (year * 10000) - ((month + 1)* 100);
@@ -77,7 +92,7 @@ class Graph extends Component {
     // get graphable array
     cumulativeData(data) {
         data = data.map(datum => {
-            return {x: -1 * this.getDaysSince(this.getDate(datum.date)), y: datum.positive}})
+            return { x: this.getDate(datum.date), y: datum.positive}})
         return data
     }
 
@@ -86,48 +101,25 @@ class Graph extends Component {
             + (12 * (endDate.getFullYear() - startDate.getFullYear()))
     }
 
+    isDataLoaded() {
+        let pointsExists = !!this.state?.graphData?.points;
+        let pointsHasData = false;
+        if (pointsExists) {
+            pointsHasData = this.state?.graphData?.points[0].length > 0;
+        }
+        return pointsHasData;
+    }
+
     render() {
-        let minDate = this.getDate(d3.min(this.state.data, function(datum) {return datum.date})) 
-        let maxDate = this.getDate(d3.max(this.state.data, function(datum) {return datum.date})) 
-        let yRaw = d3.scaleLinear()
-                    .domain(0, d3.max(this.state.data, function(datum) {return datum.positive} ))
-                    .range([300,0]);
-
-        let x = d3.scaleTime()
-                .domain([minDate, maxDate])
-                .range([0, 300]);
-        let xAxis = d3.axisBottom(x)
-                    .ticks( 1 + this.getMonthRange(minDate, maxDate))
-
-        let svg = d3.select('.Graph').append('svg').attr('width', 300).attr('height',300)
-        svg.append('g').attr('class', 'x axis').call(xAxis);
-
-        let valueLine = d3.line()
-                            .x(function(datum) {return x(this.getDate(datum?.date))})
-                            .y(function(datum) {return yRaw(datum?.positive)})
-
-        svg.append('path')
-            .attr('class', 'line')
-            .attr('d', valueLine(this.state.data))
-
-        let cumulativeData = this.cumulativeData(this.state.data);
-        let firstDerivativeData = this.cumulativeData(this.derive(this.state.data, 1));
-        let secondDerivativeData = this.cumulativeData(this.derive(this.state.data, 2));
-
         return (
             <div className="Graph">
-                <div>cumulative global cases</div>
-                <XYPlot height={300} width={300}>
-                    <LineSeries data={cumulativeData} />
-                </XYPlot>
-                <div>calculated change in global cases/day</div>
-                <XYPlot height={300} width={300}>
-                    <LineSeries data={firstDerivativeData} />
-                </XYPlot>
-                <div>acceleration of global cases/day</div>
-                <XYPlot height={300} width={300}>
-                    <LineSeries data={secondDerivativeData} />
-                </XYPlot>
+                { this.isDataLoaded() ? (
+                    <LineChart 
+                        data={this.state.graphData}
+                        width={600}
+                        height={300}
+                    />) 
+                    : ( <div>loading...</div>)}
             </div>
         );
     }
---
+++ src/LineChart.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import DataSeries from './DataSeries';
+import * as d3 from 'd3';
+
+class LineChart extends Component {
+    // constructor() {
+    //     super()
+    // }
+
+    static defaultProps = {
+        width: 600,
+        height: 300
+    }
+
+    render() {
+        let { width, height, data } = this.props;
+
+        // https://stackoverflow.com/questions/35930047/d3-line-chart-x-line-function-always-renders-nan
+        let xScale = d3.scaleTime()
+                        .domain([data.minDate, data.maxDate])
+                        .range([0, width])
+        xScale.ticks(d3.timeDay.every(5));
+
+        let yScale = d3.scaleLinear()
+                        .domain([data.yMin, data.yMax])
+                        .range([height, 10])
+
+        return (
+            <svg width={width} height={height}>
+                <DataSeries 
+                    xScale={xScale}
+                    yScale={yScale}
+                    data={data}
+                    width={width}
+                    height={height}
+                />
+            </svg>
+        )
+    }
+}
+
+export default LineChart;
