fee2634a78ca4e9379bef0be3290b9fb5e3f5d7a
david1alvarez <dalvarez@predictiveindex.com>
2020-04-29 16:45:08 -0700

    Updates

--- precache-manifest.23e4abd893c2ca41b9a9ff77d6be8578.js
+++ precache-manifest.23e4abd893c2ca41b9a9ff77d6be8578.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a03e812a3c7fe3980a4714862ff4ab2",
+    "revision": "772fa2cf5dc4a7b283e347baa93957c5",
     "url": "/covid19/index.html"
   },
   {
-    "revision": "9e2f1ee7fba2e984f921",
+    "revision": "1d027beb0efe50ab5b3f",
     "url": "/covid19/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "341aa2f13521d8da5256",
+    "revision": "999701bee4d85bd2c171",
     "url": "/covid19/static/css/main.6db80fb5.chunk.css"
   },
   {
-    "revision": "9e2f1ee7fba2e984f921",
-    "url": "/covid19/static/js/2.3ec1e286.chunk.js"
+    "revision": "1d027beb0efe50ab5b3f",
+    "url": "/covid19/static/js/2.414b1906.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/covid19/static/js/2.3ec1e286.chunk.js.LICENSE.txt"
+    "url": "/covid19/static/js/2.414b1906.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "341aa2f13521d8da5256",
-    "url": "/covid19/static/js/main.e055124f.chunk.js"
+    "revision": "999701bee4d85bd2c171",
+    "url": "/covid19/static/js/main.41f8465a.chunk.js"
   },
   {
     "revision": "bb08661687ef9ed76af2",
